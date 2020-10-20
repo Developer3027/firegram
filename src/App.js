d48cffd7449ea559e1ba28755b2ad60676379891
mason roberts <mrobe18@gmail.com>
2020-10-20 01:08:25 -0400

    created and plugged in the form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import UploadForm from './components/form/UploadForm'
 import Title from './components/title/Title'
 
 function App () {
   return (
     <div className='App'>
       <Title />
+      <UploadForm />
     </div>
   )
 }
