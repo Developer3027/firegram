c9d31672fa4014d516423be8897170c0b658de56
mason roberts <mrobe18@gmail.com>
2020-10-20 00:20:40 -0400

    initial clean up of starter code and creation of title component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './components/title/Title'
+
+function App () {
+  return (
+    <div className='App'>
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <h1>Firegram</h1>
+      <h2>Pictures</h2>
+      <p>
+        This is a line of text here at the top of the page instead of lorem.
+      </p>
+    </div>
+  )
+}
+
+export default Title
