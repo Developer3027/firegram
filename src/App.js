32cd014d64106f201bc208a6f119920db9e90d02
mason roberts <mrobe18@gmail.com>
2020-10-20 23:08:42 -0400

    created modal. condition to close modal when off click of image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UploadForm from './components/form/UploadForm'
 import ImageGrid from './components/imageGrid/ImageGrid'
+import Modal from './components/modal/modal'
 import Title from './components/title/Title'
 
 function App () {
+  const [selectedImg, setSelectedImg] = useState(null)
+
   return (
     <div className='App'>
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   )
 }
